1ef893e920438fdefdd6f8038d7edb9447ae2828
vishal <vishalvinod02@gmail.com>
2025-11-22 22:49:29 +0530

    Arrays and unions

---
+++ Practice/src/06_arrays.ts
@@ -0,0 +1,14 @@
+const superHeros:string [] = [];
+superHeros.push('Bruce Wayne','Clark Kent', 'Diana');
+console.log(superHeros);
+
+const heroPower: Array<number> = [];
+heroPower.push(2);
+
+type myUser = {
+    name: string,
+    isActive :boolean
+}
+
+const allUsers: myUser[] = [];
+allUsers.push({name: 'vishal', isActive: true});
---
+++ Practice/src/07_union.ts
@@ -0,0 +1,15 @@
+let sc:number| string = 30;
+sc=40;
+sc='ajkfhia';
+
+type nextUser ={
+    name: string;
+    _id:number
+}
+
+type Admin = {
+    username: string
+    _id: number
+}
+
+let vishal: Admin|nextUser={name:"vishal", _id:1234}
